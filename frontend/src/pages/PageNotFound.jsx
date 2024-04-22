f7232eeef1ee32d8629cafc2023c45c4c0bbf4ee
mithun20522 <mithun20522@iiitd.ac.in>
2024-04-22 17:52:43 +0530

    added framer motion

--- frontend/src/pages/PageNotFound.jsx
+++ frontend/src/pages/PageNotFound.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
-
+import {motion} from 'framer-motion';
 const PageNotFound = () => {
   return (
-    <div>PageNotFound</div>
+    <section className="flex justify-center items-center h-screen bg-slate-200">
+      <motion.div
+      initial={{ scale: 0 }}
+      animate={{ rotate: 360, scale: 1 }}
+      transition={{
+        type: "spring",
+        stiffness: 60,
+        damping: 20
+      }}    
+       className="bg-gray-100 sm:w-[20vw] sm:min-h-[30vh] text-center py-24 rounded-xl shadow-lg border w-[300px]">
+      <span className="text-xl">Error: 404</span>
+      <p className="text-md text-red-500 font-bold">Page not found</p>
+      </motion.div>
+    </section>
   )
 }
 
